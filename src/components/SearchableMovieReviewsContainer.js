f79a1fda7da2562882388efbca632f567a99c15a
Betty <betty_elfagid@yahoo.co.uk>
2021-08-01 15:12:54 +0100

    commit tree 7332537b0485c06e53f48bf02f3ab877e3fde291

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -2,8 +2,35 @@ import React, { Component } from 'react';
 import 'isomorphic-fetch';
 import MovieReviews from './MovieReviews'
 
-const NYT_API_KEY = 'your-key-here';
+const NYT_API_KEY = 'your api key';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
-            + `api-key=${NYT_API_KEY}`;
+            + `api-key=${NYT_API_KEY}&query=`;
 
-// Code SearchableMovieReviewsContainer Here
+class SearchableMovieReviewsContainer extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      searchTerm: '', reviews: []
+    }
+  }
+
+
+  submitEvent(event) {
+    event.preventDefault();
+    fetch(URL.concat(this.state.searchTerm))
+        .then(resp => resp.json())
+        .then(resp => this.setState({ reviews: resp.results }));
+  }
+
+  render(){
+    return(
+      <div className="searchable-movie-reviews">
+        <h3>The Searched Movie Reviews:</h3>
+        <MovieReviews reviews={this.state.reviews} />
+      </div>
+    );
+  }
+}
+
+export default SearchableMovieReviewsContainer;
